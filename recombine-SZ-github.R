56b3f338c8a208146f785263f65ed908ffa618b2
saschawo <wolfer@ids-mannheim.de>
2019-09-03 16:19:13 +0200

    Add files via upload

--- recombine-SZ-github.R
+++ recombine-SZ-github.R
@@ -1,11 +1,26 @@
+# Last modification: 19/09/03
+
 library(XML)
 library(RCurl)
 library(stringr)
 
-feed <- "<Insert RSS feed URL>"
+# Source 1 ----------------------------------------------------------------
+
+feed <- "<RSS feed 1>"
 feedu <- getURL(feed)
 feedp <- xmlParse(feedu, error = function (...) {}, useInternalNodes = T, options = NOCDATA)
-titles <- xpathSApply(feedp, path = "//title", xmlValue) # the 'path' argument may have to be adapted to the RSS feed entered in line 5
+titles <- xpathSApply(feedp, path = "//title", xmlValue)
+
+# Source 2 ----------------------------------------------------------------
+
+feed2 <- "<RSS feed 2"
+feedu2 <- getURL(feed2)
+feedp2 <- xmlParse(feedu2, error = function (...) {}, useInternalNodes = T, options = NOCDATA)
+titles2 <- xpathSApply(feedp2, path = "//title", xmlValue)
+
+# Integrating sources -----------------------------------------------------
+
+titles <- c(titles, titles2)
 
 # titles with exactly one colon
 titles <- titles[str_count(titles, fixed(":")) == 1]
@@ -26,40 +41,37 @@ split.titles <- split.titles[lens.left > 1 & lens.right > 1]
 left.parts <- str_trim(sapply(split.titles, FUN = function (x) x[1]))
 right.parts <- str_trim(sapply(split.titles, FUN = function (x) x[2]))
 
-# combine all left parts with all right parts
 df <- expand.grid(left.parts, right.parts)
 
 # exclude originals
 df$use <- c(rep(c(F, rep(T, length(left.parts))),
                 length(left.parts)-1), F)
+
 df <- df[df$use,]
 
-# create new headlines
 df$new <- paste(df$Var1, df$Var2, sep = ": ")
 
-# exclude already used (tweeted.txt has to exist)
+# exclude already used
 already.used <- scan("tweeted.txt", what = "char", sep = "\n")
+already.used.left <- scan("tweeted-left.txt", what = "char", sep = "\n")
+already.used.right <- scan("tweeted-right.txt", what = "char", sep = "\n")
+
 df <- df[!(df$new %in% already.used),]
+df <- df[!(df$Var1 %in% already.used.left),]
+df <- df[!(df$Var2 %in% already.used.right),]
 
-# select one new headline and write it to tweeted.txt
-selected <- sample(df$new, 1)
-write(selected, file = "tweeted.txt", append = T)
+selected.row <- sample(1:nrow(df), 1)
+selected.left <- df[selected.row, "Var1"]
+selected.right <- df[selected.row, "Var2"]
+selected.tweet <- df[selected.row, "new"]
+write(selected.tweet, file = "tweeted.txt", append = T)
+write(selected.left, file = "tweeted-left.txt", append = T)
+write(selected.right, file = "tweeted-right.txt", append = T)
 
-# tweet (Twitter developer account needed, replace with your credentials
 library(twitteR)
-setup_twitter_oauth(consumer_key = "<KEY>",
-                    access_token = "<TOKEN>",
-                    consumer_secret = "<SECRET>",
-                    access_secret = "<TOKEN SECRET>")
+setup_twitter_oauth(consumer_key = "<key>",
+                    access_token = "<token>",
+                    consumer_secret = "<secret>",
+                    access_secret = "<token secret>")
 
-updateStatus(selected)
-
-# @BotSchlagzeilen tweets every 3 hours. This is done by a simple shell script. It looks like this:
-#
-# while true;
-# do
-# Rscript recombine-SZ.R 
-# sleep 10800
-# done
-#
-# @BotSchlagzeilen runs on a Raspberry Pi 3
+updateStatus(selected.tweet)
